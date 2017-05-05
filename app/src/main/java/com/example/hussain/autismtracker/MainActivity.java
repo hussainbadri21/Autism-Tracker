59829485e1c6d5f7160bba0a36d7fd2d94cefec7
hussainHMC <hussainbadri786@gmail.com>
2017-05-06 04:54:21 +0530

    First Commit

---
+++ app/src/main/java/com/example/hussain/autismtracker/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.hussain.autismtracker;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Timer mTimer = null;
+    private Handler mHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (mTimer != null) {
+            mTimer.cancel();
+        } else {
+            // recreate new
+            mTimer = new Timer();
+        }
+        // schedule task
+        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, 60 * 1000);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class TimeDisplayTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            // run on another thread
+            mHandler.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    // display toast
+                    //Toast.makeText(getApplicationContext(), "Timer Run",Toast.LENGTH_SHORT).show();
+                    // if(sharedPreferences.getBoolean("cameraValue",false))
+                    startService(new Intent(MainActivity.this, DemoCamService.class));
+                    //sendData();
+                }
+
+            });
+        }
+    }
+}
